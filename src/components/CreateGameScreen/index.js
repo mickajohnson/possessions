ff7f6991c786746236bb82d4a4d49d7a97ff72f1
Mick Johnson <20012080+mickajohnson@users.noreply.github.com>
2021-02-28 17:52:10 -0600

    well, I tried

--- src/components/CreateGameScreen/index.js
+++ src/components/CreateGameScreen/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 
 export default function CreateGameScreen({ onJoin, lobbyClient }) {
   const [name, setName] = React.useState("");
@@ -17,25 +18,89 @@ export default function CreateGameScreen({ onJoin, lobbyClient }) {
 
   const buttonDisabled = name.length === 0 || !numberOfPlayers;
   return (
-    <div>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <select
-        value={numberOfPlayers}
-        onChange={(e) => setNumberOfPlayers(e.target.value)}
-      >
-        {[2, 3, 4].map((val) => (
-          <option key={val} value={val}>
-            {val}
-          </option>
-        ))}
-      </select>
+    <Container>
+      <FormContainer>
+        <Label htmlFor="name">Your Name</Label>
+        <NameInput
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Label htmlFor="numberOfPlayers">Number of Players</Label>
+        <Select
+          name="numberOfPlayers"
+          value={numberOfPlayers}
+          onChange={(e) => setNumberOfPlayers(e.target.value)}
+        >
+          {[2, 3, 4].map((val) => (
+            <option key={val} value={val}>
+              {val}
+            </option>
+          ))}
+        </Select>
 
-      <button
-        disabled={buttonDisabled}
-        onClick={() => handleCreateGame(name, numberOfPlayers)}
-      >
-        Create Game
-      </button>
-    </div>
+        <Button
+          disabled={buttonDisabled}
+          onClick={() => handleCreateGame(name, numberOfPlayers)}
+        >
+          Create Game
+        </Button>
+      </FormContainer>
+    </Container>
   );
 }
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: fit-content;
+`;
+
+const Container = styled.div`
+  padding: 3em;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const NameInput = styled.input`
+  height: 3em;
+  border-radius: 5px;
+  border: none;
+  margin-bottom: 3em;
+  width: 100%;
+  padding: 0 1em;
+  font-family: "Staatliches";
+`;
+
+const Select = styled.select`
+  height: 3em;
+  margin-bottom: 3em;
+  border-radius: 5px;
+  border: none;
+  padding: 0 1em;
+  cursor: pointer;
+  width: 100%;
+  font-family: "Staatliches";
+`;
+
+const Button = styled.button`
+  width: 100%;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.colors.blue};
+  border: none;
+  height: 2em;
+  border-radius: 5px;
+  font-family: "Staatliches";
+  font-size: 1.5em;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  font-size: 2em;
+`;
