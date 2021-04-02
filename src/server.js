8012386e1439fe32e9c1a1656880c5d44ea0c274
Mick Johnson <20012080+mickajohnson@users.noreply.github.com>
2021-04-01 19:47:26 -0500

    trying pg

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import { Server } from "boardgame.io/server";
 import path from "path";
 import serve from "koa-static";
+import { PostgresStore } from "bgio-postgres";
 import NightStandStuff from "./game/index";
 
-const server = Server({ games: [NightStandStuff] });
+const db = new PostgresStore(process.env.DATABASE_URL);
+
+const server = Server({ games: [NightStandStuff], db });
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
