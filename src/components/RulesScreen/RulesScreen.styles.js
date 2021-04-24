99270e6511ac6b82a34edf7f90b8d46dc78ade8e
Mick Johnson <20012080+mickajohnson@users.noreply.github.com>
2021-04-24 16:08:22 -0500

    fonts

--- src/components/RulesScreen/RulesScreen.styles.js
+++ src/components/RulesScreen/RulesScreen.styles.js
@@ -2,8 +2,20 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   font-family: "Domine";
+  padding: 30px 100px;
+
+  h1 {
+    font-family: "Staatliches";
+  }
 
   h2 {
     color: var(--color-yellow);
+    padding: 25px 0 10px;
+    font-family: "Staatliches";
+  }
+
+  p {
+    line-height: 1.5;
+    padding-bottom: 10px;
   }
 `;
--- src/components/RulesScreen/index.js
+++ src/components/RulesScreen/index.js
@@ -28,14 +28,14 @@ export default function RulesScreen() {
         goals' relationships in the opposite direction of that listed on the
         goal, you will just get zero points (not points taken away).
       </p>
-      <h3>Show Goal Card here</h3>
+      {/* <h3>Show Goal Card here</h3> */}
       <h2>Goal Selection</h2>
       <p>
         At the beginning of the game, players are dealt four goals each (three
         in a four player game). Players select the one that they don't want and
         discard it.
       </p>
-      <h3>Show Goal Selection Screen here</h3>
+      {/* <h3>Show Goal Selection Screen here</h3> */}
       <h2>Initial Board State</h2>
       <p>
         Each game of Possessions will start in a unique way. The nine rooms are
@@ -48,7 +48,7 @@ export default function RulesScreen() {
         room, Grandpa - Grandpa’s room, Mom - Parent’s Room, Dad - Bathroom
       </p>
       <p>Each relationship starts at zero.</p>
-      <h3>Show Initial board here with arrows toward what things are</h3>
+      {/* <h3>Show Initial board here with arrows toward what things are</h3> */}
       <h2>Rounds</h2>
       <p>
         Rounds consist of two phases: the planning phase, where players lay down
@@ -88,41 +88,51 @@ export default function RulesScreen() {
       <h2>Actions</h2>
       <ul>
         <li>
-          <b>Move character</b>: Choose a character to move, then choose which
-          room they will move to. They must move the number of spaces listed on
-          the card.
+          <p>
+            <b>Move character:</b> Choose a character to move, then choose which
+            room they will move to. They must move the number of spaces listed
+            on the card.
+          </p>
         </li>
         <li>
-          <b>Leave behind something.</b>: Choose a character to leave behind a
-          Drop Token in the room they are currently in. The size and polarity of
-          of the token is listed on the card. This represents something the
-          character is leaving behind for another character to discover, be it
-          positive (like tidying up a room) or negative (like leaving dirty
-          dishes lying around). Players are encouraged to announce what it is
-          that's being left. The idea is that a larger number represents a more
-          positive or negative thing.
+          <p>
+            <b>Leave behind something:</b> Choose a character to leave behind a
+            Drop Token in the room they are currently in. The size and polarity
+            of of the token is listed on the card. This represents something the
+            character is leaving behind for another character to discover, be it
+            positive (like tidying up a room) or negative (like leaving dirty
+            dishes lying around). Players are encouraged to announce what it is
+            that's being left. The idea is that a larger number represents a
+            more positive or negative thing.
+          </p>
         </li>
         <li>
-          <b>React to something.</b> Choose a character to interact with the
-          Drop Tokens left behind by another character in the room they are
-          currently in. Choose which pile of Drop Tokens they'll be interacting
-          with. If multiple tokens from one character are in a room, the effect
-          will be the total of the tokens. The corresponding relationship will
-          change that amount. This represents a character reacting to things
-          another character left in the room. Again, players are encouraged to
-          describe this reaction. If there are no characters in a room with
-          anther character's drop tokens, the player's turn is skipped.
+          <p>
+            <b>React to something:</b> Choose a character to interact with the
+            Drop Tokens left behind by another character in the room they are
+            currently in. Choose which pile of Drop Tokens they'll be
+            interacting with. If multiple tokens from one character are in a
+            room, the effect will be the total of the tokens. The corresponding
+            relationship will change that amount. This represents a character
+            reacting to things another character left in the room. Again,
+            players are encouraged to describe this reaction. If there are no
+            characters in a room with anther character's drop tokens, the
+            player's turn is skipped.
+          </p>
         </li>
         <li>
-          <b>Convo (Fight/Bond)</b>: First choose whether the two characters
-          you'll be selecting will fight (negatively affecting the relationship
-          by one) or bond (positively affecting the relationship by 1). Choose
-          the two characters that you want to interact. The two must be in the
-          same room. If this is a fight, choose a character to storm off, then
-          choose an adjacent room for them to storm off to. If this is a bond,
-          they'll remain in the same room. Once again, players are encouraged to
-          describe the specifics of the interaction. If there are no two
-          characters in a room together the player's turn is skipped.
+          <p>
+            <b>Convo (Fight/Bond):</b> First choose whether the two characters
+            you'll be selecting will fight (negatively affecting the
+            relationship by one) or bond (positively affecting the relationship
+            by 1). Choose the two characters that you want to interact. The two
+            must be in the same room. If this is a fight, choose a character to
+            storm off, then choose an adjacent room for them to storm off to. If
+            this is a bond, they'll remain in the same room. Once again, players
+            are encouraged to describe the specifics of the interaction. If
+            there are no two characters in a room together the player's turn is
+            skipped.
+          </p>
         </li>
       </ul>
       <h2>Game End</h2>
